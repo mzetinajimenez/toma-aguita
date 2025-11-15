ce96c15f99b8405b3fea62e1c87ff2d4327c3da7
Marvin Zetina-Jimenez <marvin.zetinaj@gmail.com>
2025-11-15 14:56:57 -0500

    Initial Commit

---
+++ toma-aguita/toma_aguitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  toma_aguitaApp.swift
+//  toma-aguita
+//
+//  Created by Marvin Zetina on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct toma_aguitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ toma-aguitaTests/toma_aguitaTests.swift
@@ -0,0 +1,17 @@
+//
+//  toma_aguitaTests.swift
+//  toma-aguitaTests
+//
+//  Created by Marvin Zetina on 11/15/25.
+//
+
+import Testing
+@testable import toma_aguita
+
+struct toma_aguitaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
